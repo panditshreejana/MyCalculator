22a7e1fdcb282c9ce4b5465311d1c5a451d92145
panditshreejana <shreejanapandit98469@gmail.com>
2023-01-23 14:10:57 +0545

    myCalculator final

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -86,22 +86,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-//test
-
-
-
-
-//test1
    String getResult(String data) {
         try{
             Context context= Context.enter();
